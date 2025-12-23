c6f6fe66518f8dbd8df5f13c3a0154446e37eb25
rifqiyy19 <rifqicpz@gmail.com>
2025-12-24 02:22:48 +0700

    Update gui (menambahkan fitur tampilan login user & admin

--- src/main/java/com/hotel/database/AuthManager.java
+++ src/main/java/com/hotel/database/AuthManager.java
@@ -21,10 +21,7 @@ public class AuthManager {
     }
 
     private void initializeDefaultUsers() {
-        // User default
-        users.add(new User(nextUserId++, "admin", "admin123", "admin@baobab.com", "ADMIN", "Administrator"));
-        users.add(new User(nextUserId++, "user", "user123", "user@baobab.com", "USER", "Guest User"));
-        users.add(new User(nextUserId++, "guest", "guest123", "guest@baobab.com", "USER", "Tam Safari"));
+        users.add(new User(nextUserId++, "user", "user123", "user@gmail.com", "USER", "Guest User"));
     }
 
     public User authenticate(String username, String password) {
@@ -32,23 +29,5 @@ public class AuthManager {
                 .filter(user -> user.getUsername().equals(username) && user.getPassword().equals(password))
                 .findFirst()
                 .orElse(null);
-    }
-
-    public boolean registerUser(String username, String password, String email, String fullName) {
-        // Cek username sudah ada
-        boolean usernameExists = users.stream()
-                .anyMatch(user -> user.getUsername().equals(username));
-
-        if (usernameExists) {
-            return false;
-        }
-
-        User newUser = new User(nextUserId++, username, password, email, "USER", fullName);
-        users.add(newUser);
-        return true;
-    }
-
-    public List<User> getAllUsers() {
-        return new ArrayList<>(users);
     }
 }
